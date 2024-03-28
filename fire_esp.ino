00b71302b56348497277bced4fafba05f7a4fc16
Abhishank Saran <97939389+abh1shank@users.noreply.github.com>
2024-03-29 01:02:56 +0530

    Add files via upload

--- fire_esp.ino
+++ fire_esp.ino
@@ -2,38 +2,68 @@
 
 #define buzzerPin 23
 #define servoPin 21
-
+#define count 20
 Servo servoMotor;
-
+int deadline;
 void setup() {
   pinMode(buzzerPin, OUTPUT); 
   pinMode(LED_BUILTIN, OUTPUT);
   stopBuzzer();
   servoMotor.attach(servoPin);
+  servoMotor.write(0);
+  deadline_restore(count);
   Serial.begin(9600);
 }
 
 void loop() {
   if (Serial.available()) {
     char incomingByte = Serial.read();
-    Serial.println(incomingByte); // Print the incoming byte to the serial monitor
-    if (incomingByte == '1') {
-      digitalWrite(LED_BUILTIN, HIGH);
-      servoMotor.write(180);
-      soundBuzzer();
-      delay(200); // Delay for 200 milliseconds
-      stopBuzzer();
+    Serial.println(incomingByte); 
+    if (incomingByte == 'f') 
+    {
+        deadline--;
+        delayed_trips(150);
+    } 
+    else if (incomingByte == 'p') 
+    {
+        deadline--;
+        delayed_trips(250);
+    }
+    else if(incomingbyte == 's')
+    {
+        deadline--;
+        delayed_trips(500);
+    }
+    else 
+    {
+      deadline_restore();
       digitalWrite(LED_BUILTIN, LOW);
+      stopBuzzer();
       servoMotor.write(0);
-      delay(200); // Delay for 200 milliseconds
-    } else {
-      digitalWrite(LED_BUILTIN, LOW);
-      stopBuzzer();
-      servoMotor.write(0); // Stop servo motor immediately
     }
   }
 }
-
+void deadline_restore()
+{
+    deadline=count;
+}
+void delayed_trips(int time)
+{
+      if
+      digitalWrite(LED_BUILTIN, HIGH);
+      soundBuzzer();
+      delay(time); 
+      digitalWrite(LED_BUILTIN, LOW);
+      stopBuzzer();
+      delay(time);  
+}
+void trip_MCB()
+{
+      deadline_restore();
+      digitalWrite(LED_BUILTIN, LOW);
+      stopBuzzer();
+      servoMotor.write(180);
+}
 void soundBuzzer() {
   digitalWrite(buzzerPin, LOW); // Power the buzzer
 }
